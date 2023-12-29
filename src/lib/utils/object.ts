fc9af4efa7b8c8bb9ab0a18cfd6a3a26b7616f36
Meonako <dubi.db28@gmail.com>
2023-12-29 16:01:24 +0700

    I don't know what I'm doing

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -18,9 +18,11 @@ export function setMissingValueFromDefault(
     def: Record<string, any>
 ): Record<string, any> {
     for (const [name, task] of Object.entries(def)) {
-        if (typeof task == "object") {
+        if (typeof target[name] == 'undefined') {
+            target[name] = task;
+        } else if (typeof task == "object") {
             target[name] = setMissingValueFromDefault(target[name], task);
-        } else if (target[name]) {
+        } else if (typeof target[name] != 'undefined') {
             continue;
         } else {
             target[name] = task;
